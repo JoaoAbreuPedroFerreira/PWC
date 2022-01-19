0aa9d7595b37b000561c16bb0504dfcb3a7096e8
JoaoPedroAbreuFerreira <2211889@my.ipleiria.pt>
2022-01-19 22:42:35 +0000

    Funcionalidade que retorna o utilizador à homepage em caso de erro

--- Detalhes/detalhes.js
+++ Detalhes/detalhes.js
@@ -4,43 +4,45 @@ var selectedCoin = urlParams.get("selectedCoin");
 
 if(selectedCoin)
 {
-    var currency = urlParams.get("currency");
-    populateDetailsPage(currency, selectedCoin);
+    apiRequest();
 }
 else
 {
-    //sem moeda selecionada
+    window.location.replace("../index.html");
 }
 
-function populateDetailsPage(currency, selectedCoin)
+function apiRequest()
+{
+   var url = `https://api.coingecko.com/api/v3/coins/${selectedCoin}`;
+
+   var xhr = new XMLHttpRequest();
+   xhr.open("GET", url);
+
+   xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+         //console.log(xhr.status);
+         //console.log(JSON.parse(xhr.response));
+         res = JSON.parse(xhr.response);
+         if(res.error)
+         {
+            window.location.replace("../index.html");
+         }
+         else
+         {
+            populateDetailsPage(res);
+         } 
+      }
+   };
+
+   xhr.send();
+}
+
+function populateDetailsPage(coin)
 {   
-    var currencySymbol;
-
-    switch(currency)
-    {
-    case "usd" : currencySymbol = "$"; break;
-    case "eur" : currencySymbol = "€"; break;
-    case "jpy" : currencySymbol = "¥"; break;
-    }
-
-    var url = `https://api.coingecko.com/api/v3/coins/${selectedCoin}`;
-
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === 4) {
-            //console.log(xhr.status);
-            console.log(JSON.parse(xhr.response));
-            coin = JSON.parse(xhr.response);
-            $(".descricao p").html(coin.description["en"]);
-            $(".crypto_img").attr("src", coin.image.large);
-            $(".nome").text(coin.name);
-            $(".ranking").text(coin.market_cap_rank + " #");
-            $(".valor").text(coin.market_data.current_price[currency] + " " + currencySymbol);
-            $(".variacao").text(coin.market_data.market_cap_change_percentage_24h.toFixed(2) + " %");
-        }
-    };
-
-    xhr.send();
+    $(".descricao p").html(coin.description["en"]);
+    $(".crypto_img").attr("src", coin.image.large);
+    $(".nome").text(coin.name);
+    $(".ranking").text(coin.market_cap_rank + " #");
+    $(".valor").text(coin.market_data.current_price[currency] + " " + currencySymbol);
+    $(".variacao").text(coin.market_data.market_cap_change_percentage_24h.toFixed(2) + " %");
 }
