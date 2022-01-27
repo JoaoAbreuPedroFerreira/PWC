0b2dc24790ad66a960337d57e9b19c8641e8279d
JoaoPedroAbreuFerreira <2211889@my.ipleiria.pt>
2022-01-27 17:57:03 +0000

    Reestruturação do código para corresponder aos estilos e mudança para requests com ajax

--- script.js
+++ script.js
@@ -6,19 +6,12 @@ function apiRequest()
 {
    var url = `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=true`;
 
-   var xhr = new XMLHttpRequest();
-   xhr.open("GET", url);
-
-   xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4) {
-         //console.log(xhr.status);
-         console.log(JSON.parse(xhr.response));
-         res = JSON.parse(xhr.response);
+   $.ajax({
+      type: "GET",
+      url: url,
+   }).done(function(res){
          populateList(res);
-      }
-   };
-
-   xhr.send();
+   });
 }
 
 function populateList(coins)
@@ -29,41 +22,41 @@ function populateList(coins)
 
    for(i = 0; i < coins.length; i++)
    {
-      console.log(i);
-      var coin = $(document.createElement("div")).attr("id", "coinRow").attr("data-coin-name", coins[i].name).attr("data-coin-id", coins[i].id).attr("data-coin-symbol", coins[i].symbol).addClass("row");
-      var logo = $(document.createElement("div")).append(`<img class="coinLogo" src=${coins[i].image} style="width:50px; height:50px"/>`).addClass("col-sm");
-      var rank = $(document.createElement("div")).append(`<span class="coinRank">#${i+1}</span>`).addClass("col-sm");
-      var name = $(document.createElement("div")).append(`<span class="coinName">${coins[i].name}</span>`).addClass("col-sm");
-      var symbol = $(document.createElement("div")).append(`<span class="coinSymbol">${coins[i].symbol.toUpperCase()}</span>`).addClass("col-sm");
-      var price = $(document.createElement("div")).append(`<span class="coinPrice">${coins[i].current_price} ${currencySymbol}</span>`).addClass("col-sm");
+      var coin = $(document.createElement("tr")).attr("data-coin-name", coins[i].name).attr("data-coin-id", coins[i].id).attr("data-coin-symbol", coins[i].symbol);
+
+      var rank = $(document.createElement("td")).append(`<span class="coinRank">${i+1}</span>`).attr("scope", "row").addClass("clickable");
+      var logo = $(document.createElement("td")).append(`<img class="coinLogo" src=${coins[i].image} style="width:50px; height:50px"/>`).addClass("clickable");
+      var nameAndSymbol = $(document.createElement("td")).append(`<span class="coinName">${coins[i].name}</span> <span class="coinSymbol">(${coins[i].symbol.toUpperCase()})</span>`).addClass("clickable");
+      var price = $(document.createElement("td")).append(`<span class="coinPrice">${coins[i].current_price} ${currencySymbol}</span>`).addClass("clickable");
 
       var changeIn24hValue = coins[i].market_cap_change_percentage_24h.toFixed(2);
-      changeIn24hValue > 0 ? changeIn24hValue = `▲ ${changeIn24hValue}` : changeIn24hValue = `▼ ${changeIn24hValue}`;
-      var changeIn24h = $(document.createElement("div")).append(`<span class="coinChange">${changeIn24hValue} %</span>`).addClass("col-sm");
 
-      var marketcap = $(document.createElement("div")).append(`<span class="coinCap">${coins[i].market_cap} ${currencySymbol}</span>`).addClass("col-sm");
+      if(changeIn24hValue > 0)
+      {
+         var changeIn24h = $(document.createElement("td")).append(`<span class="coinChange">▲ ${changeIn24hValue} %</span>`).css("color", "#00bc8c").addClass("clickable");
+      }
+      else
+      {
+         var changeIn24h = $(document.createElement("td")).append(`<span class="coinChange">▼ ${changeIn24hValue} %</span>`).css("color", "#e74c3c").addClass("clickable");
+      }
+      
+      var marketcap = $(document.createElement("td")).append(`<span class="coinCap">${coins[i].market_cap} ${currencySymbol}</span>`).addClass("clickable");
 
       var sparklineValues = coins[i].sparkline_in_7d.price.slice(coins[i].sparkline_in_7d.price.length - 24);
-      var sparklineGraph = $(document.createElement("div")).append('<span class="coinSparkline"></span>');
+      var sparklineGraph = $(document.createElement("td")).append('<span class="coinSparkline"></span>').addClass("clickable");
 
-      var favoriteButton = $(document.createElement("button")).attr("id", coins[i].id).addClass("favoriteButton").attr("onclick", "toggleFavorite(this)");
+      var favoriteButton = $(document.createElement("i")).attr("id", coins[i].id).addClass("favoriteButton far fa-heart fa-2x").attr("onclick", "toggleFavorite(this)");
 
       if(favorites.indexOf(coins[i].id) > -1)
       {
-         favoriteButton.addClass("favorite");
+         favoriteButton.toggleClass("favorite far fas");
       }
 
-      var favoriteButtonDiv = $(document.createElement("div")).append(favoriteButton);
-
-      //var info = $(document.createElement("a"));
-      //info.text(`#${rank} ${name} (${symbol}) | ${price} ${currencySymbol} | ${changeIn24H}% | ${marketcap} ${currencySymbol}`);
-      //info.attr("href", `./Detalhes/detalhes.html?selectedCoin=${coins[i].id}`);
-      //info.append(sparklineGraph);
-      
+      var favoriteButtonDiv = $(document.createElement("td")).append(favoriteButton);
 
       sparklineGraph.sparkline(sparklineValues.map(sparkLinePrice => sparkLinePrice.toFixed(2)), {width: "72px", height: "34px"});
 
-      coin.append(logo, rank, name, symbol, price, changeIn24h, marketcap, sparklineGraph, favoriteButtonDiv);
+      coin.append(rank, logo, nameAndSymbol, price, changeIn24h, marketcap, sparklineGraph, favoriteButtonDiv);
 
       list.append(coin);
    }
@@ -71,17 +64,32 @@ function populateList(coins)
    coinsList.append(list);
    
    $.sparkline_display_visible();
+
+   $(".clickable").on("click", redirect);
+
+   if($("#searchBar").val() != "")
+   {
+      search($("#searchBar"));
+   }
 }
 
 function toggleTop(e)
 {
+   $(e.target).text() == "Show Top 100" ? $(e.target).text("Show Top 10") : $(e.target).text("Show Top 100");
+
    var quantity = $(e.target).val();
    var coinsList = $("#coinsList");
-   $(coinsList).children("div").toggle();
+   $(coinsList).children("tr").toggle();
 
    for (i = 0; i < quantity; i++) 
    {
-      var coin = $(coinsList).children("div")[i];
+      var coin = $(coinsList).children("tr")[i];
       $(coin).toggle();
    }
+}
+
+function redirect(e)
+{
+   coinId = $($(e.target).parents()[0]).attr("data-coin-id");
+   window.location = `./Detalhes/detalhes.html?&currency=${currency}&selectedCoin=${coinId}`;
 }
